07b8f2301ae65fd07372dd1313a9405d5e18fbce
Bernhard Dressel <dresber@users.noreply.github.com>
2021-04-09 19:40:01 +0200

    adopt updating of display so that only changed sections are updated

--- src/display/display.cpp
+++ src/display/display.cpp
@@ -76,7 +76,7 @@ void setup_display(void)
 
     lcd.home();
 
-    print_arrow_forward();    
+//    print_arrow_forward();    
 }
 
 
@@ -85,8 +85,6 @@ void setup_display(void)
  */
 void update_display(void)
 {
-    lcd.clear();
-
 #ifdef SAFETY
     print_safety_state();
     print_remote_state();
@@ -118,58 +116,78 @@ static void print_arrow_forward(void)
 #ifdef SAFETY
 static void print_safety_state(void)
 {
+    static SafetyState prev_state;
     SafetyState act_state = get_safety_state();
 
-    if (act_state == OK)
+    if (prev_state != act_state)
     {
-        lcd.setCursor(11, 3);
-        lcd.print("OK");
-    }
-    else if (act_state == HALT)
-    {
+        prev_state = act_state;
+
         lcd.setCursor(10, 3);
-        lcd.print("HALT");
-    }
-    else
-    {
-        lcd.setCursor(10, 3);
-        lcd.print("STOP");
+        lcd.print("    ");
+
+        if (act_state == OK)
+        {
+            lcd.setCursor(11, 3);
+            lcd.print("OK");
+        }
+        else if (act_state == HALT)
+        {
+            lcd.setCursor(10, 3);
+            lcd.print("HALT");
+        }
+        else
+        {
+            lcd.setCursor(10, 3);
+            lcd.print("STOP");
+        }
     }
 }
 
 
 static void print_remote_state(void)
 {
+    static RemoteState prev_state;
     RemoteState act_state = get_remote_state();
-    
-    lcd.setCursor(15, 3);
-    lcd.write(remote_antenna_signe_left.char_nr);
-    lcd.write(remote_antenna_signe_right.char_nr);
-    lcd.setCursor(17, 3);
 
-    if(act_state == MONITOR)
+    if (prev_state != act_state)
     {
-        lcd.printstr("MON");
-    }
-    else if (act_state == CONTROL)
-    {
-        lcd.printstr("CON");
-    }
-    else
-    {
-        lcd.printstr("OFF");
-    }
+        lcd.setCursor(15, 3);
+        lcd.write(remote_antenna_signe_left.char_nr);
+        lcd.write(remote_antenna_signe_right.char_nr);
+        lcd.setCursor(17, 3);
 
+        if(act_state == MONITOR)
+        {
+            lcd.printstr("MON");
+        }
+        else if (act_state == CONTROL)
+        {
+            lcd.printstr("CON");
+        }
+        else
+        {
+            lcd.printstr("OFF");
+        }
+    }
 }
 #endif // #ifdef SAFETY
 
 #ifdef POWER_MONITOR
 static void _print_power_level(void)
 {
-    lcd.setCursor(19, 0);
-    lcd.print("%");
-    lcd.setCursor(15, 0);
-    lcd.print(get_actual_power_level());
+    static int16_t prev_power_level;
+    int16_t act_power_level = get_actual_power_level();
+
+    if (prev_power_level != act_power_level)
+    {
+        prev_power_level = act_power_level;
+
+        lcd.setCursor(19, 0);
+        lcd.print("%");
+        lcd.setCursor(15, 0);
+        lcd.print(get_actual_power_level());
+    }
 }
 #endif // #ifdef POWER_MONITOR
 
@@ -177,14 +195,24 @@ static void _print_power_level(void)
 static void _print_addons_control(void)
 {
     static bool blink_toggle = false;
+    static bool prev_light_state = true;
 
-    lcd.setCursor(7, 0);
-    lcd.write(light_left.char_nr);
+    bool act_light_state = get_light_state();
 
-    if(get_light_state())
+    if (prev_light_state != act_light_state)
     {
-        lcd.setCursor(8, 0);
-        lcd.write(light_beams_right.char_nr);
+        prev_light_state = act_light_state;
+
+        lcd.setCursor(7, 0);
+        lcd.print("  ");
+        lcd.setCursor(7, 0);
+        lcd.write(light_left.char_nr);
+
+        if(act_light_state)
+        {
+            lcd.setCursor(8, 0);
+            lcd.write(light_beams_right.char_nr);
+        }
     }
 
     lcd.setCursor(11, 0);
