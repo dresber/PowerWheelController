ddf56a1651b3b2375a80f53d5be3bee1f0497e9e
Bernhard Dressel <dresber@users.noreply.github.com>
2021-04-12 21:55:10 +0200

    add battery level state refactor methods to use them separate

--- src/sensors/voltage_monitor.cpp
+++ src/sensors/voltage_monitor.cpp
@@ -6,7 +6,11 @@
 // ------------------------------------------------ //
 //                  definitions
 // ------------------------------------------------ //
-
+typedef enum
+{
+    BATTERY_TOO_LOW = 0,
+    BATTERY_OK = 1,
+}BatteryLevelState;
 
 // ------------------------------------------------ //
 //                  type definitions
@@ -16,6 +20,7 @@
 // ------------------------------------------------ //
 //                  global vars
 // ------------------------------------------------ //
+static BatteryLevelState _battery_state = BATTERY_TOO_LOW;
 
 
 // ------------------------------------------------ //
@@ -26,12 +31,35 @@
 // ------------------------------------------------ //
 //              function definitions
 // ------------------------------------------------ //
+/**
+ * 
+ */
 int16_t get_actual_power_level(void)
+{
+    uint32_t main_voltage = get_actual_battery_voltage();
+    uint16_t power_level = 0;
+
+#ifdef DEBUG_POWER_MONITOR
+    DEBUG_OUTPUT.print("main_voltage: ");
+    DEBUG_OUTPUT.print(main_voltage);
+    DEBUG_OUTPUT.println(" mV");
+#endif // #ifdef DEBUG_POWER_MONITOR
+
+    if (main_voltage > MAIN_POWER_MIN_VOLTAGE)
+    {
+        power_level = (main_voltage - MAIN_POWER_MIN_VOLTAGE) * 100 / (MAIN_POWER_MAX_VOLTAGE - MAIN_POWER_MIN_VOLTAGE);
+    }
+
+    return (power_level);
+}
+
+/**
+ * 
+ */
+int16_t get_actual_battery_voltage(void)
 {
     uint32_t adc_raw_value = analogRead(MAIN_POWER_PIN);
     uint32_t adc_voltage = 0;
-    uint32_t main_voltage = 0;
-    uint16_t power_level = 0;
 
 #ifdef DEBUG_POWER_MONITOR
     DEBUG_OUTPUT.print("adc_raw_value: ");
@@ -46,18 +74,41 @@ int16_t get_actual_power_level(void)
     DEBUG_OUTPUT.println(" mV");
 #endif // #ifdef DEBUG_POWER_MONITOR
 
-    main_voltage = (R1_VALUE * adc_voltage / R2_VALUE) + adc_voltage;
-
 #ifdef DEBUG_POWER_MONITOR
     DEBUG_OUTPUT.print("main_voltage: ");
-    DEBUG_OUTPUT.print(main_voltage);
+    DEBUG_OUTPUT.print(((R1_VALUE * adc_voltage / R2_VALUE) + adc_voltage));
     DEBUG_OUTPUT.println(" mV");
 #endif // #ifdef DEBUG_POWER_MONITOR
 
-    if (main_voltage > MAIN_POWER_MIN_VOLTAGE)
-    {
-        power_level = (main_voltage - MAIN_POWER_MIN_VOLTAGE) * 100 / (MAIN_POWER_MAX_VOLTAGE - MAIN_POWER_MIN_VOLTAGE);
-    }
+    return (R1_VALUE * adc_voltage / R2_VALUE) + adc_voltage;
+}
 
-    return (power_level);
+/**
+ * 
+ */
+bool check_if_battery_level_is_ok_to_drive(void)
+{
+    uint32_t main_voltage = get_actual_battery_voltage();
+
+    if (_battery_state == BATTERY_TOO_LOW)
+    {
+        if (main_voltage > MIN_ACTIVAtION_VOLTAGE)
+        {
+            _battery_state = BATTERY_OK;
+            return (true);
+        }
+        return (false);    
+    }
+    else
+    {
+        if (main_voltage > MIN_DRIVE_VOLTAGE)
+        {
+            return (true);
+        }
+        else
+        {
+            _battery_state = BATTERY_TOO_LOW;
+            return (false);
+        }
+    }
 }
--- src/sensors/voltage_monitor.h
+++ src/sensors/voltage_monitor.h
@@ -14,6 +14,9 @@
 #define MAIN_POWER_MAX_VOLTAGE 14000
 #define MAIN_POWER_MIN_VOLTAGE 11000
 
+#define MIN_DRIVE_VOLTAGE 11500
+#define MIN_ACTIVAtION_VOLTAGE 11800
+
 #define ADC_RESOLUTION 1024
 #define ADC_VOLTAGE_IN_UV 5000000
 
@@ -33,6 +36,9 @@
 //              function prototypes
 // ------------------------------------------------ //
 int16_t get_actual_power_level(void);
+int16_t get_actual_battery_voltage(void);
+
+bool check_if_battery_level_is_ok_to_drive(void);
 
 
 #endif // #ifndef voltage_H
